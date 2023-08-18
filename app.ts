244cf80f3593f9de30d49fefc13cd553802f8153
naushadraien <naushadraien5@gmail.com>
2023-08-18 16:31:48 +0545

    Typescript Learning

---
+++ app.ts
@@ -0,0 +1,41 @@
+let variable: string = "John";
+console.log(variable);
+
+(variable = "hey"), console.log(variable);
+
+let testString: string;
+
+testString = "hello";
+
+// testString = 5; // error
+
+let testNumber: number;
+
+testNumber = 5;
+
+// testNumber = 'hello'; // error
+
+let testBoolean: boolean;
+
+testBoolean = true;
+
+// testBoolean = 'hello'; // error
+
+let testStringOrNumber: string | number;
+
+testStringOrNumber = "hello";
+testStringOrNumber = 34;
+
+//Array
+let namePerson = ["Harry", "Potter", "Lol"];
+
+// namePerson.push(34)
+namePerson.push("King");
+
+let number = [34, 54, 53];
+// number.push('hello')
+number.push(23);
+
+
+
+
